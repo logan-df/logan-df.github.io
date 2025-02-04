b5686981a780e490380fa1e3d7af883679c9bde9
logan-df <ldford711@gmail.com>
2025-02-04 17:00:09 -0500

    wireframe

---
+++ csce242/exercises/04jsintro/script-arrow-functions.js
@@ -0,0 +1,10 @@
+//Version 1 where we learned arrow functions
+const doStuff = () => {
+    console.log("doing stuff");
+}
+
+//Version 2
+
+
+//do stuff when button clicked
+document.getElementById("btn-click-me").onclick = doStuff;
