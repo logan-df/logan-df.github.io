d23175800d2b8a19e45543bdc550a0c9dafa5f04
logan-df <ldford711@gmail.com>
2025-02-08 23:34:10 -0500

    what ive got

---
+++ csce242/assignments/assignment05/script.js
@@ -0,0 +1,19 @@
+/*Creating a paragraph element with content: hello*/
+document.getElementById("text-generator").onclick = () => {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "hello";
+    document.getElementById("text-generator").appendChild(paragraph);
+}
+
+/*Setting the initial color*/
+document.getElementById("star").style.backgroundColor = document.getElementById("color-picker").value;
+
+/*JS code found on: https://www.geeksforgeeks.org/how-to-change-an-element-color-based-on-value-of-the-color-picker-value-using-onclick/ */
+document.getElementById("color-picker").onchange = () => {
+    document.getElementById("star").style.backgroundColor = document.getElementById("color-picker").value;
+}
+
+/*Change image on click*/
+document.getElementById("image").onclick = () => {
+    document.getElementById("image").src = 'https://picsum.photos/200' + new Date().getTime();
+}
